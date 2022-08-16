c7220b2ceee2a11afd1cc8925fe3411cf5de5bf0
Anton Momot <anton.momot@lanars.com>
2022-08-16 14:38:38 +0300

    tsx changed

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,91 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+interface MailingOperations {
+    writeLetter(): void
+    sendLetter(): void
+    receiveLetter(): void
+    readLetter(): void
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// Context
+class Mailer implements MailingOperations {
+    private _state: MailerState = new InitialState();
+
+    constructor() {
+        this.logCurrentState()
+    }
+
+    public writeLetter(): void {
+        this._state = this._state.writeLetter();
+        this.logCurrentState()
+    }
+    public sendLetter(): void {
+        this._state = this._state.sendLetter();
+        this.logCurrentState()
+    }
+    public receiveLetter(): void {
+        this._state = this._state.receiveLetter();
+        this.logCurrentState()
+    }
+    public readLetter(): void {
+        this._state = this._state.readLetter();
+        this.logCurrentState()
+    }
+
+    private logCurrentState(): void {
+        alert(this._state.constructor.name)
+    }
+}
+
+class MailerState implements MailingOperations {
+    public writeLetter(): MailerState {
+        throw new Error("Invalid operation")
+    }
+    public sendLetter(): MailerState {
+        throw new Error("Invalid operation")
+    }
+    public receiveLetter(): MailerState {
+        throw new Error("Invalid operation")
+    }
+    public readLetter(): MailerState {
+        throw new Error("Invalid operation")
+    }
+}
+
+class InitialState extends MailerState {
+    public writeLetter(): MailerState {
+        return new LetterHasBeenWritten()
+    }
+}
+
+class LetterHasBeenWritten extends MailerState {
+    public sendLetter(): MailerState {
+        return new LetterSending()
+    }
+}
+
+class LetterSending extends MailerState {
+    public receiveLetter(): MailerState {
+        return new LetterRecived()
+    }
+}
+
+class LetterRecived extends MailerState {
+    public readLetter(): MailerState {
+        return new InitialState()
+    }
+}
+
+const mailer = new Mailer();
+
+mailer.writeLetter();
+
+mailer.sendLetter();
+
+mailer.receiveLetter();
+
+mailer.readLetter();
+
+
 reportWebVitals();
